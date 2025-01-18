f69e31746ad858152275a1c749600c9f8460d820
Torge Schwark <torge.schwark@outlook.de>
2025-01-18 18:35:38 +0100

    testing with classification

--- training-code/helper_functions.py
+++ training-code/helper_functions.py
@@ -105,3 +105,27 @@ def get_num_clases(prices_file_path):
         # Finde den Schlüssel mit der höchsten ID (größte ID)
     max_id = max(prices.keys())
     return max_id
+
+def get_combined_dict(prices_file_path):
+    prices = {}
+    with open(prices_file_path, "r") as f:
+        for line in f:
+            # Extrahiere das Dictionary aus der Zeile
+            if ": " in line:
+                _, dict_str = line.strip().split(": ", 1)
+                try:
+                    current_dict = ast.literal_eval(dict_str)
+                    for key, value in current_dict.items():
+                        if key in prices:
+                            # Überprüfen, ob Werte übereinstimmen
+                            if prices[key] != value:
+                                print(f"Warnung: Konflikt für Schlüssel {key}: "
+                                    f"{prices[key]} vs {value}")
+                        else:
+                            # Schlüssel hinzufügen
+                            prices[key] = value
+                except Exception as e:
+                    print(f"Fehler beim Verarbeiten der Zeile: {line}\n{e}")
+
+        # Finde den Schlüssel mit der höchsten ID (größte ID)
+    return prices
