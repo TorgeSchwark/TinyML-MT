966c4ee20cc74faf074ca0d8bfbb5c801e0d6ed2
XgamerTV <matsaustralia@yahoo.com.au>
2025-05-02 12:28:07 +0200

    Fixed paths, created pipeline workflow

--- comfyui.sh
+++ comfyui.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
 source ComfyUI/.comfyui/bin/activate
-python ComfyUI/main.py --disable-metadata --user-directory "/data22/stu235269/TinyML-MT/code_imagegeneration/comfyui" --cuda-device 3
+python ComfyUI/main.py --disable-metadata --output-directory "huggingface/ai_shelf" --user-directory "code_imagegeneration/comfyui" --cuda-device 3
 
 read -p "Press enter to exit"
