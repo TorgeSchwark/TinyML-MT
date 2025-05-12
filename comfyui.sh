08440854eb1ce0fdf3640b5cc78ce8071afa047c
XgamerTV <matsaustralia@yahoo.com.au>
2025-05-12 11:45:26 +0200

    Comfyui  fix, added directions to pipeline (testing wasnt so promising but lets see)

--- comfyui.sh
+++ comfyui.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env zsh
 
 source ComfyUI/.comfyui/bin/activate
+export HF_HOME="./.cache"
 python ComfyUI/main.py --disable-metadata --output-directory "huggingface/ai_shelf" --user-directory "code_imagegeneration/comfyui" --cuda-device 3
 
 read -p "Press enter to exit"
