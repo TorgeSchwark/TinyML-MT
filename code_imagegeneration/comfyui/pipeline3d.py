09e96f9138af8dfbaf5ad507eaa89277c7d1376b
XgamerTV <matsaustralia@yahoo.com.au>
2025-06-04 20:52:02 +0200

    pipeline changes, new preprocessing

--- code_imagegeneration/comfyui/pipeline3d.py
+++ code_imagegeneration/comfyui/pipeline3d.py
@@ -2,9 +2,7 @@ import subprocess
 import os
 from tqdm import tqdm
 import random
-from rembg import remove
-from PIL import Image
-import io
+import shutil
 
 PATH = "huggingface/testing"
 SCRIPT = "threestudio.sh"
@@ -22,30 +20,25 @@ for folder in FOLDERS:
                 img_path = os.path.join(root, file)
                 print(f"Processing image: {img_path}")
 
-                with open(img_path, 'rb') as i:
-                    input_data = i.read()
-
-                img = Image.open(io.BytesIO(input_data))
-                # If image has a background (not fully opaque RGBA), remove background and save as new file with _rgba
-                used_img_path = img_path
-                if REMOVE_BG:
-                    if not (img.mode == 'RGBA' and all(a == 255 for a in img.getchannel('A').getdata())):
-                        print(f"Removing background from {img_path}")
-                        output_data = remove(input_data)
-                        base, ext = os.path.splitext(img_path)
-                        new_img_path = f"{base}_rgba{ext}"
-                        with open(new_img_path, 'wb') as o:
-                            o.write(output_data)
-                        used_img_path = new_img_path
-
+                # Execute Threestudio command
                 new_seed = random.getrandbits(64)
-                command = f"./threestudio.sh --config {CONFIG} --train --gpu 2 data.image_path=\"../{used_img_path}\" exp_root_dir=\"../{PATH}\" seed={new_seed}"
-                output_dir = os.path.join(root, os.path.splitext(file)[0])
-                os.makedirs(output_dir, exist_ok=True)
-                generated_dir = os.path.join("mvimg-gen-zero123-sai", f"{os.path.splitext(file)[0]}_rgba_rgba.png@20250604-184721", "save", "it0-test")
-                if os.path.isdir(generated_dir):
-                    for gen_file in os.listdir(generated_dir):
-                        src = os.path.join(generated_dir, gen_file)
-                        dst = os.path.join(output_dir, gen_file)
-                        os.rename(src, dst)
+                command = f"./{SCRIPT} --config {CONFIG} --train --gpu 2 data.image_path=\"../{img_path}\" exp_root_dir=\"../{PATH}\" seed={new_seed}"
                 subprocess.run(command, shell=True, executable="/bin/zsh")
+
+                # Find the newest folder in mvimg-gen-zero123-sai
+                mvimg_dir = os.path.join(PATH,"mvimg-gen-zero123-sai")
+                if os.path.isdir(mvimg_dir):
+                    print("FOUND")
+                    subfolders = [os.path.join(mvimg_dir, d) for d in os.listdir(mvimg_dir) if os.path.isdir(os.path.join(mvimg_dir, d))]
+                    if subfolders:
+                        latest_folder = max(subfolders, key=os.path.getctime)
+                        it0_test_dir = os.path.join(latest_folder, "save", "it0-test")
+                        if os.path.isdir(it0_test_dir):
+                            output_dir = os.path.join(root, os.path.splitext(file)[0])
+                            os.makedirs(output_dir, exist_ok=True)
+                            for gen_file in os.listdir(it0_test_dir):
+                                src = os.path.join(it0_test_dir, gen_file)
+                                dst = os.path.join(output_dir, gen_file)
+                                os.rename(src, dst)
+                    # Delete the mvimg-gen-zero123-sai folder completely
+                    shutil.rmtree(mvimg_dir)
